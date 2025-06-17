aab6bcef5dd42092835ecebbe35cbe32a1e37429
Manish Aryal <manisharyal490@gmail.com>
2025-06-17 12:46:05 +0530

    update

--- src/main/java/org/example/Day2Pro1.java
+++ src/main/java/org/example/Day2Pro1.java
@@ -1,4 +1,36 @@
 package org.example;
 
-public class Day2Pro1 {
+import java.util.Scanner;
+public class Day2Pro1{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("First Number:");
+        int x = sc.nextInt();
+        System.out.println("Second Number:");
+        int y = sc.nextInt();
+
+        System.out.println("enter Choice:");
+        String Choice = sc.next();
+        switch(Choice) {
+            case "+":
+                System.out.println("Sum is :"+ (x+y));
+                break;
+            case "-":
+                System.out.println("Diff is :"+ (x-y));
+                break;
+            case "*":
+                System.out.println("Product is :"+ (x*y));
+                break;
+            case "/":
+                System.out.println("Division is :"+ (x/y));
+                break;
+            default:
+
+                System.out.println("Invalid Choice!");
+
+        }
+
+
+
+    }
 }
